1bad3b928a5e15290f1fd3bb2cf1a5f30d6bb252
Cuakero <cuakmo@gmail.com>
2026-01-27 20:24:25 -0300

    Prueba
    Prueba de Menu Principal

---
+++ Assets/Scripts/PaseDeNivel.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PaseDeNivel : MonoBehaviour
+{
+    public void Cambio()
+    {
+        SceneManager.LoadScene("Prueba 2");
+    }
+}
