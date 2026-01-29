aef1319526c14059040bbc81b1124bfa29728df4
Gian Franco <giancapo2011@gmail.com>
2026-01-29 01:16:40 -0300

    Generación de pista

---
+++ Assets/Scripts/GeneracionPista.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GeneracionPista : MonoBehaviour
+
+{
+    public GameObject Player;
+    public GameObject[] calles;
+
+    private List<GameObject> callesInstanciadas = new List<GameObject>();
+
+    public float posZ;
+    public float posY;
+
+    [SerializeField] private int[] ordenGeneracion;
+    private int indiceSecuencia = 0;
+
+    void Update()
+    {
+        GenerarPista();
+        EliminarCallesViejas();
+    }
+
+    public void GenerarPista()
+    {
+        if (Player.transform.position.z > posZ - 100)
+        {
+            int indicePrefab = ordenGeneracion[indiceSecuencia];
+
+            GameObject nueva = Instantiate(
+                calles[indicePrefab],
+                new Vector3(0, posY, posZ + 87.7f),
+                Quaternion.identity
+            );
+
+            callesInstanciadas.Add(nueva);
+
+            posZ += 50;
+
+            indiceSecuencia++;
+
+            // si llega al final, frena o reinicia (tu elección)
+            if (indiceSecuencia >= ordenGeneracion.Length)
+                indiceSecuencia = 0; // o eliminar esta línea si NO querés loop
+        }
+    }
+
+    void EliminarCallesViejas()
+    {
+        float distanciaDestruir = 120f;
+
+        for (int i = callesInstanciadas.Count - 1; i >= 0; i--)
+        {
+            if (Player.transform.position.z - callesInstanciadas[i].transform.position.z > distanciaDestruir)
+            {
+                Destroy(callesInstanciadas[i]);
+                callesInstanciadas.RemoveAt(i);
+            }
+        }
+    }
+
+
+}
+
